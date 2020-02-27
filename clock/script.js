ebff7c0f6dddc08be62220b774532a89a936082d
myamc <47002324+myamc@users.noreply.github.com>
2020-02-27 18:08:56 -0500

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".warhol2").addClass("alarm");
+    $(".rset").addClass("alarm");
+    $(".seconds1").addClass("alarm");
+  }, 8000);
+});
