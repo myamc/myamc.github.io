9c468c35c2a145e43733dcb8317a80d5d6a78200
myamc <47002324+myamc@users.noreply.github.com>
2020-04-02 16:02:19 -0500

    Add files via upload

--- camplaz/demo/script.js
+++ camplaz/demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 7500, npcCollisionRadius: 100, actionDuration:900, jumpHeight: 300, npcMoveToX: 200, npcSpeed: 150,});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 7500, npcCollisionRadius: 100, actionDuration:900, jumpHeight: 300, npcSpeed: 250, maxSimultaneousNPCs: 4, removeNPCAfterInteractedDelay: 1000});
 });
